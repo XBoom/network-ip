169c5dc828df2de32e7c723d3563f1c81901a3bd
XBDove <728081687@qq.com>
2023-12-07 14:08:29 +0800

    log

--- libs/lib_log/lib_log.h
+++ libs/lib_log/lib_log.h
@@ -1,3 +1,5 @@
+#ifndef __LIB_LOG_H__
+#define __LIB_LOG_H__
 
 //log_fmt_t 日志结构
 typedef struct log_fmt_t
@@ -11,16 +13,30 @@ typedef struct log_fmt_t
     va_list args;       //可变参数
 }log_fmt_t;
 
-/*
-    @brief 输出日志
-    @param 
-        to_type 输出到那里
-        log_
-*/
+//输出日志
 void log_output(uint32_t to_type, uint32_t log_type, uint32_t level,
                     const char *file, uint32_t line_no, const char *func, 
                     const char *info, const char *fmt, ...)
 {
-    va_list ap; 
-    va_start(ap, fmt);
 }
+
+//输出到tcp
+// static void tcp_printf(cl_tcp *tcp, const char* fmt, ...)
+// {
+// #ifdef CTEST    //如果是单元测试，直接退出
+//     return;
+// #endif
+//     va_list ap;
+//     char buf[DEFAULT_WRITE_SIZE];
+//     memset(buf, 0, DEFAULT_WRITE_SIZE);
+
+//     va_start(ap, fmt);
+//     vsprintf_n(buf, DEFAULT_WRITE_SIZE, fmt, ap);
+//     va_end(ap);
+
+//     buf[DEFAULT_WRITE_SIZE - 1] = '\0';
+
+//     //cl_tcp_syn_write(tcp, buf, strlen(buf));
+// }
+
+#endif
