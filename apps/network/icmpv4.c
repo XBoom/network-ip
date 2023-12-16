f075d9b396f1946ae6d673a0fe0bda18d97cde9e
XBDove <728081687@qq.com>
2023-12-16 17:56:03 +0800

    1216

--- apps/network/icmpv4.c
+++ apps/network/icmpv4.c
@@ -3,9 +3,32 @@
 #include "ip.h"
 #include "utils.h"
 
+//处理  ICMP_V4_ECHO
+void icmpv4_reply(struct sk_buff *skb)
+{
+    struct ip_hdr *ip_h = ip_hdr_init(skb);
+    struct icmp_ipv4 *icmp;
+
+    //
+    
+}
+
 void icmpv4_incoming(struct sk_buff *skb)
 {
     struct iphdr *iphdr = ip_hdr(skb);
     //__((packed))__ 直接转换
     struct icmp_v4 *icmp = (struct icmp_v4 *) iphdr->data;
+
+    switch(icmp->type)
+    {
+        case:ICMP_V4_ECHO:
+            icmpv4_reply(skb);
+        break;
+        default:
+            print_error("not support icmp type %" PRIu8 "\n", icmp->type);
+            goto drop_skb;
+    }
+drop_skb:
+    free_skb(skb);
+    skb = NULL;
 }
--- libs/lib_uv/uv.h
+++ libs/lib_uv/uv.h
@@ -7,4 +7,15 @@ struct _uv_queue {
   struct _uv_queue* prev;
 };
 
+struct uv_loop_s {
+    void *data; //用户自定义数据
+
+    unsigned int active_handles;    //长请求
+    struct uv__queue handle_queue;  //链表
+    union{
+        void* unused;  
+        unsigned int count;
+    }active_reqs;   //短请求
+}
+
 #endif
