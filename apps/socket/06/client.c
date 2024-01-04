cc54bf6166f2488c650cfc9290d078364240e882
XBoom <728081687@qq.com>
2024-01-04 22:58:04 +0800

    回声 CS

--- apps/socket/06/client.c
+++ apps/socket/06/client.c
@@ -17,22 +17,30 @@ int main(int argc, char *argv[])
     //2. 设置地址
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(argv[0]);
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
     server_addr.sin_port = htons(atoi(argv[2]));
 
     //3. 发起连接
-    ret = connect(client_sock, (const sturct sockaddr *)&server_addr, sizeof(server_addr));
+    ret = connect(client_sock, (const struct sockaddr *)&server_addr, sizeof(server_addr));
     CHECK_RET(ret == -1, "connect failed");
 
     while(1)    
     {
-        char message[MAX_BUF_LEN] = "client message";
-        ret = write(client_sock, message, strlen(message));
-        CHECK_RET(ret = -1, "write failed");
+        char message[MAX_BUFF_LEN] = {0};
+        fgets(message, MAX_BUFF_LEN, stdin);
+        CHECK_RET(strcmp(message, "Q\n") == 0, "exit normal");
+
+        LOG_INFO("input message:%s", message);
+        ret = write(client_sock, message, strlen(message) - 1);
+        CHECK_RET(ret == -1, "write failed");
 
         char reponse[MAX_BUFF_LEN] = {0};
         ret = read(client_sock, &reponse, MAX_BUFF_LEN);
         CHECK_RET(ret == -1, "read failed");
+        LOG_INFO("reponse message:%s", reponse);
+
+        memset(&message, 0, sizeof(message));
+        memset(&reponse, 0, sizeof(reponse));
     }
     close(client_sock);
     return 0;
--- apps/socket/06/server.c
+++ apps/socket/06/server.c
@@ -1,13 +1,13 @@
 #include "common.h"
 
 #define MAX_BUF_LEN (1024)
+#define CLIENT_NUM (5)
 
 int main(int argc, char *argv[])
 {
-    CHECK_RET(argc != 2, "Usage:server <ip> <port>");
+    CHECK_RET(argc != 2, "Usage:server <port>");
 
     int ret = 0;
-    int str_len = 0;
     int read_len = 0;
 
     int server_sock = 0;
@@ -23,8 +23,8 @@ int main(int argc, char *argv[])
     //2. 设置地址
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    server_addr.sin_port = htons(atoi(argv[2]));
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
 
     //3. 绑定地址
     ret = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
@@ -34,22 +34,28 @@ int main(int argc, char *argv[])
     ret = listen(server_sock, 5);
     CHECK_RET_GOTO(ret == -1, out, "listen failed");
 
-    //6. 接收连接
-    client_addr_len = sizeof(client_addr);
-    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
-    CHECK_RET_GOTO(client_sock == -1, out, "accept failed");
-
-    //7. 接收并回复
     char message[MAX_BUF_LEN] = {0};
-    while(1)
+    for(int i = 0; i < CLIENT_NUM; i++)
     {
-        if((read_len = read(client_sock, message, MAX_BUF_LEN)) > 0)
+        //5. 接收连接
+        client_addr_len = sizeof(client_addr);
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+        CHECK_RET_GOTO(client_sock == -1, out, "accept failed");
+        LOG_INFO("accept socket %d", client_sock);
+
+        //6. 读取信息
+        while((read_len = read(client_sock, message, MAX_BUF_LEN)) > 0)
         {
+            LOG_INFO("recv message:%s", message);
+
+            //7. 返回消息
             ret = write(client_sock, message, strlen(message));
             CHECK_RET_GOTO(ret == -1, out, "write failed");
 
             LOG_INFO("reponse len %d message:%s", strlen(message), message);
         }
+        close(client_sock);
+        memset(&client_addr, 0, sizeof(client_addr));
     }
     LOG_INFO("reponse message end");
 
