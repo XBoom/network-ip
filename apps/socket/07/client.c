b424d47833b38c77ca84c822c329d417530e14f0
XBDove <728081687@qq.com>
2024-01-06 12:57:23 +0800

    UDP回声函数

--- apps/socket/07/client.c
+++ apps/socket/07/client.c
@@ -1,30 +1,43 @@
+#include "common.h"
 
+#define MAX_BUFF_LEN (1024)
 
+int main(int argc, char *argv[])
+{
+    CHECK_RET(argc!= 3, "Usage:client <ip> <port>");
 
-#include <sys/socket.h>
+    int ret = 0;
+    int server_sock = 0;
+    struct sockaddr_int server_addr;
 
-/**
- * @brief 使用UDP套接字进行发送 
- * @param 
- *      sock 用于传输数据的UDP套接字文件描述符
- *      buff 保存待传输数据的缓冲地址值
- *      nbytes 待传输的数据长度，以B为单位
- *      flags 可选参数、没有则为0
- *      to 存有目的地址信息的sockaddr解耦提变量的地址值
- *      addrlen 参数to的地址值结构体变量长度
- * @return 成功返回传输的字节数，失败时返回-1
-*/
-ssize_t sendto(int sock, void *buff, size_t nbytes, int flags,
-                    struct sockaddr *to, socklen_t addrlen);
+    server_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    CHECK_RET(server_sock, "socket failed");
 
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_port = htons(atoi(argv[2]));
 
-/// @brief 使用UDP套接字接收数据
-/// @param sock 用于接收数据的UDP套接字文件描述符
-/// @param buff 保存接收数据的缓冲地址值
-/// @param nbytes   可接收的最大字节数，故无法超过参数buff所指的缓冲大小 
-/// @param flags 可选项参数，若没有则传入0
-/// @param from  存有发送端地址信息的sockaddr结构体变量的地址值
-/// @param addrlen 保存参数from的结构体变量长度的变量值
-/// @return 
-ssize_t recvfrom(int sock, void *buff, size_t nbytes, int flags,
-                    struct sockaddr *from, socklen_t *addrlen);
+    char message[MAX_BUFF_LEN] = 0;
+    char response[MAX_BUFF_LEN] = 0;
+    sockaddr_len_t server_addr_len = 0;
+    while(1)
+    {
+        fgets(message, MAX_BUFF_LEN - 1, stdin);
+        LOG_INFO("client send %s to %s", message, argv[1]);
+        ret = sendto(server_sock, message, MAX_BUFF_LEN - 1, 0,
+            (struct sockaddr*)&server_addr, sizeof(server_addr));
+        CHECK_RET_GOTO(ret < 0, out, "send to failed");
+        
+        ret = recvfrom(server_sock, resonse, MAX_BUFF_LEN - 1, 0, 
+            (struct sockaddr *)&server_addr, &server_addr_len);
+        CHECK_RET_GOTO(ret < 0, out, "recvfrom failed");
+        LOG_INFO(client recv %s from %s, response, argv[1]);
+
+        memset(message, 0, sizeof(message));
+        memset(resonse, 0, sizeof(resonse));
+    }
+out:
+    close(server_sock);
+    return 0;
+}
--- apps/socket/07/server.c
+++ apps/socket/07/server.c
@@ -2,6 +2,42 @@
 
 int main(int argc, char *argv[])
 {
-    
+    CHECK_RET(argc != 2, "Usage:server <port>");
+
+    int ret = 0;
+    int server_sock = 0;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+
+    server_addr = socket(AF_INET, SOCK_DGRAM, 0);
+    CHECK_RET(server_addr == -1, "socket failed");
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+
+    ret = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    CHECK_RET(ret != 0, "bind failed");
+
+    char message[MAX_BUFF_LEN] = {0};
+    sockaddr_size_t client_addr_len = sizeof(client_addr);
+    while(1)
+    {
+        ret = recvfrom(server_sock, message, MAX_BUFF_LEN, 0, 
+            (struct sockaddr *)&client_addr, &client_addr_len);
+        CHECK_RET_GOTO(ret < 0, out, "recv from failed");
+
+        LOG_INFO("recv message %s from %s", message, inet_ntoa(client_addr));
+
+        ret = sendto(server_sock, message, sizeof(message), 0, 
+            (struct sockaddr *)&client_addr, sizeof(client_addr));
+        CHECK_RET_GOTO(ret < 0, out, "send to failed");
+
+        memset(message, 0, sizeof(message));
+    }
+out:
+    close(server_sock);
+    close(client_sock);
     return 0;
 }
