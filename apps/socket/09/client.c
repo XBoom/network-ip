6f84d985abd9a7bf12267b521bd72de300367b8b
XBoom <728081687@qq.com>
2024-01-11 23:17:15 +0800

    套接字断开

--- apps/socket/09/client.c
+++ apps/socket/09/client.c
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
     int ret = 0;
     int client_fd = 0;
     int server_sock = 0;
-    int send_sock = 0;
     struct sockaddr_in server_addr;
 
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -20,26 +19,29 @@ int main(int argc, char *argv[])
     server_addr.sin_addr.s_addr = inet_addr(argv[1]);
     server_addr.sin_port = htons(atoi(argv[2]));
 
-    send_sock = connect(server_sock, (const struct sockaddr*)&server_addr, sizeof(server_addr));
-    CHECK_RET(send_sock == -1, "connect failed");
+    ret = connect(server_sock, (const struct sockaddr*)&server_addr, sizeof(server_addr));
+    CHECK_RET(ret == -1, "connect failed");
 
     client_fd = open("client.txt", O_RDWR|O_CREAT);
     CHECK_RET_GOTO(client_fd == -1, out, "open failed");
 
     int read_len = 0;
     char message[MAX_BUFF_LEN] = {0};
-    while((read_len = read(send_sock, message, MAX_BUFF_LEN)))
+    while((read_len = read(server_sock, message, MAX_BUFF_LEN)))
     {
         ret = write(client_fd, message, sizeof(message));
         CHECK_RET_GOTO(ret == -1, out, "fwrite failed");
     }
+    //尝试提前关闭读取
+    ret = shutdown(server_sock, SHUT_RD);
+    CHECK_RET_GOTO(ret == -1, out, "shutdown rd failed");
+
     char reponse[MAX_BUFF_LEN] = "recv all file";
-    ret = write(send_sock,reponse, sizeof(reponse));
-    CHECK_RET_GOTO(ret == -1, "write failed");
+    ret = write(server_sock,reponse, sizeof(reponse));
+    CHECK_RET_GOTO(ret == -1, out, "write failed");
     
 out:
     close(client_fd);
     close(server_sock);
-    close(send_sock);
     return 0;
 }
--- apps/socket/09/server.c
+++ apps/socket/09/server.c
@@ -1,10 +1,67 @@
-#include <sys/socket.h>
+#include "common.h"
 
 #define MAX_BUFF_LEN (1024)
+#define SERVER_TXT_PATH "./09/server.txt"
 
 int main(int argc, char *argv[])
 {
-    
-    return 0;
+    CHECK_RET(argc != 2, "Usage: server <port>");
 
+    int ret = 0;
+    int server_sock = 0;
+    int client_sock = 0;
+    FILE* server_fd = 0;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = 0;
+
+    //构建套接字
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    CHECK_RET(server_sock == -1, "socket failed");
+
+    //设置服务端地址
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+
+    //设置服务端套接字地址
+    ret = bind(server_sock, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+    CHECK_RET(ret == -1, "bind failed");
+
+    //监听
+    ret = listen(server_sock, 5);
+    CHECK_RET(ret == -1, "listen failed");
+
+    char buff[MAX_BUFF_LEN] = {0};
+    client_addr_len = sizeof(client_addr);
+    while (1)
+    {
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+        CHECK_RET_GOTO(client_sock == -1, out, "accept failed");
+
+        server_fd = fopen(SERVER_TXT_PATH, "r");
+        CHECK_RET_GOTO(server_fd == NULL, out, "fopen failed");
+
+        while(fgets(buff, sizeof(buff), server_fd))
+        {
+            ret = write(client_sock, buff, sizeof(buff));
+            CHECK_RET_GOTO(ret == -1, out, "write failed");
+        }
+        //关闭写
+        shutdown(client_sock, SHUT_RD);
+
+        memset(buff, 0 , sizeof(buff));
+        while(read(client_sock, buff, sizeof(buff)))
+        {
+            LOG_INFO("server recv %s", buff);
+        }
+        LOG_INFO("end");
+        //关闭读
+        //shutdown(client_sock, SHUT_WR);
+    }
+out:
+    close(server_sock);
+    fclose(server_fd);
+    return 0;
 }
