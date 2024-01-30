130e2d2c6a7a480480ce25fec155ac5ef6420dc5
XBDove <728081687@qq.com>
2024-01-30 20:36:12 +0800

    0130

--- apps/socket/14/server.c
+++ apps/socket/14/server.c
@@ -1,44 +1,53 @@
-#include <signal.h>
+#include "common.h"
 
-/**
-* 返回类型：void (*)(int)，表示返回一个指向参数为整型、返回类型为 void 的函数的指针
-* 函数名：signal，是函数的名称。
-* 参数列表：(int signo, void (*func)(int))，包括两个参数：
-    signo：整型，表示信号的编号。
-    (*func)(int)：一个指向参数为整型、返回类型为 void 的函数的指针，用于指定处理该信号时要调用的函数
-*/
-//void (*signal(int signo, void (*func)(int)))(int);
+#define MAX_BUFF_LEN (1024)
 
-
-/**
- * signo 信号信息
- * act 对应与第一个参数的信号处理函数
- * oldact 通过次参数获取之前注册的信号处理函数指针，若不需要则传递 0
-*/
-// int sigaction(int signo, const struct sigaction *act, struct sigaction * oldact);
-
-// struct  sigaction {
-// 	union __sigaction_u __sigaction_u;  /* signal handler */
-// 	sigset_t sa_mask;               /* signal mask to apply */
-// 	int     sa_flags;               /* see signal options below */
-// };
-
-
-/**
-常用的处理信号有
-SIGALRM: 已经通过调用alarm函数注册的时间
-         通过 alarm 函数注册的时间表示：在接收到 SIGALRM 信号之前的秒数。当调用 alarm(seconds) 后，系统会在指定的秒数后发送 SIGALRM 信号给调用进程
-SIGINT: 输入 CTRL + C
-SIGCHLD: 子进程终止
-*/
-
-
-//31#include <unistd.h>
-
-//返回 0 或者已秒为单位的距 SIGALRM 信号发生所剩实践
-//unsigned int alarm(unsigned int seconds);
-
-int main()
+int main(int argc, char *argv[])
 {
+    CHECK_RET(argc != 2, "Usage: server port");
+
+    int ret = 0;
+    int server_socket = 0;
+    int client_socket = 0;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+
+    //1. 创建套接字
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    CHECK_RET(server_socket == -1, "socket failed");
+
+    //2. 注册一个信号
+    
+
+    //2. 设置地址
+    memset(server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+
+    //3. 绑定地址
+    
+
+    //4. listen
+    
+    while(1)
+    {
+        //5. accept
+        client_socket = accept();
+        CHECK_RET(client_socket == -1, "accept failed");
+
+        //6. 构建子进程进行处理
+        pid_t child_pid = fork();
+        CHECK_RET_GOTO(child_pid < 0, end, "fork failed");
+
+        if(child_pid == 0)  //子进程
+        {
+
+        }
+
+    }
+
+end:
+
     return 0;
 }
