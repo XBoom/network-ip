3c2dfa008175b6f83decdc3e171cda2f5c64a85b
XBoom <728081687@qq.com>
2024-03-28 22:04:51 +0800

    proto 升成消息传输格式

--- apps/ipcs/02/01.c
+++ apps/ipcs/02/01.c
@@ -1,6 +1,16 @@
 #include "fifo.h"
+#include "request.pb-c.h"
 
 #define CLIENT_FIFO_CLIENT_PATH "./client/%d/send"
+
+// 显示响应消息
+void show_response_message(FifoResponse *resp)
+{
+    FifoRespHead *head = resp->head;
+
+    LOG_INFO("head: %d %d %d", head->id, head->code, head->code);
+    LOG_INFO("body: %s", resp->body);
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,9 +21,15 @@ int main(int argc, char *argv[])
     fifo_msg req;
     fifo_msg resp;
 
-    ret = mkfifo(CLIENT_FIFO_CLIENT_PATH, USR_WRITE); // 写权限
+    pid_t pid = 0;
+    // 1. 根据当前进程 id 设置管道路径
+    char *fifo_path = sprintf(CLIENT_FIFO_CLIENT_PATH, getpid());
+
+    // 2. 构建管道
+    ret = mkfifo(fifo_path, USR_WRITE); // 写权限
     CHECK_RET(ret != 0, "mkfifo failed");
 
+    // 3. 构建子进程
     int child_pid = fork();
     CHECK_RET(child_pid < 0, "fork failed");
     if (child_pid == 0)
@@ -33,8 +49,20 @@ int main(int argc, char *argv[])
         LOG_INFO("child exit");
         return 0;
     }
-    client_fd = open(CLIENT_FIFO_CLIENT_PATH, O_WRONLY);
-    ret = send(client_fd, "hello", MSG_MAX_LEN);
+    // 4. 构造一个消息
+    uint8_t buf[MSG_MAX_LEN];
+    FifoRequest *request = FIFO_REQUEST__INIT();
+    request->head->id = getpid();
+    strcpy(request->head->fifo, fifo_path);
+    request->head->len = 0;
+    int len = fifo_request__pack_to_buffer(request, buf, sizeof(buf));
+
+    // 5. 打开服务端监听管理，把消息写进去
+    client_fd = open(SERVER_FIFO_PATH_NAME, O_WRONLY);
+    CHECK_RET(client_fd < 0, "open fifo failed");
+
+    // 6. 监听消息
+    ret = write(client_fd, buf, MSG_MAX_LEN);
     waitpid(-1, &status, WNOHANG);
     LOG_INFO("parent exit");
     return 0;
